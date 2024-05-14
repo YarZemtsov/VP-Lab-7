67b0b70828ebed5b9fc2f91478d86936a8f2be73
”Zemtsov <yarosz@inbox.com>
2024-05-14 18:57:25 +0300

    add class Weather

---
+++ WeatherService/Weather.cpp
@@ -0,0 +1,19 @@
+#include "Weather.h"
+Weather::Weather(string city, double lon, double lat, double temperature, string weather, double windSpeed, int clouds) {
+	this->city = city;
+	this->lon = lon;
+	this->lat = lat;
+	this->temperature = temperature;
+	this->weather = weather;
+	this->windSpeed = windSpeed;
+	this->clouds = clouds;
+}
+ostream& operator << (ostream& out, Weather inf) {
+	out << inf.city << ":\n";
+	out << "   Lon          " << inf.lon << "\n";
+	out << "   Lat          " << inf.lat << "\n";
+	out << "   Temperature  " << inf.temperature << "\n";
+	out << "   Weather      " << inf.weather << "\n";
+	out << "   Wind speed   " << inf.windSpeed << "\n";
+	out << "   Clouds       " << inf.clouds << "\n";
+}
---
+++ WeatherService/Weather.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Weather
+{
+	string city; //  Киров
+	double lon; // 49.6601
+	double lat; // 58.5966
+	double temperature; // 5.69
+	std::string weather; // дождь
+	double windSpeed; // 4.27
+	int clouds; // 100
+	Weather(string city, double lon, double lat, double temperature, string weather, double windSpeed, int clouds);
+	friend ostream& operator << (ostream& out, Weather inf);
+};
+
