b11a28e751a148293da2216f9d930938d4e87fde
”Zemtsov <yarosz@inbox.com>
2024-05-15 11:04:37 +0300

    test done, Debug ignored

--- WeatherService/WeatherService.cpp
+++ WeatherService/WeatherService.cpp
@@ -2,10 +2,14 @@
 //
 
 #include <iostream>
-
+#include "Service.h"
+#include "JsonService.h"
+#include "Weather.h"
 int main()
 {
-    std::cout << "Hello World!\n";
+    JsonService js;
+    Weather w = js.getWeather("weather.json");
+    cout << w;
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
